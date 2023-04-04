d3e30028d786ef75860502c7524913ab46622755
Chisonline <chisonline.cn@gmail.com>
2023-04-04 20:30:52 +0800

    4.4

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,9 @@
 project "ImGui"
     kind "StaticLib"
-    lauguage "C++"
+    language "C++"
 
     targetdir ("bin" .. outputdir .. "/%{prj.name}")
-    objdir {"bin/int" .. outputdir .. "/%{prj.name}"}
+    objdir ("bin/int" .. outputdir .. "/%{prj.name}")
 
     files {
         "imconfig.h",
@@ -23,5 +23,24 @@ project "ImGui"
     cppdialect "C++17"
     staticruntime "On"
 
-    filter { "system:windows","configuration:Release"}
-        buildoptions "/MT"
+    filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
